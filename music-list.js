a4c3f0b5286acd1fa906693692e910cf652db5dc
Mood-Song <157859706+Mood-Song@users.noreply.github.com>
2024-01-27 08:41:54 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,68 @@
+let allMusic = [
+    {
+        name: "Party With A Jagaban",
+        artist: "Midas The Jagaban",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "No Lie (Speed Up)",
+        artist: "Sean Paul Ft Dua Lipa",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "PERADABAN",
+        artist: "Feast",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Replay (Speed Up)",
+        artist: "Iyaz",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Karena Kamu (Speed Up)",
+        artist: "Geisha",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Prom Queen (Speed Up)",
+        artist: "Beach Bunny",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Kill Bill",
+        artist: "SZA",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "HEAT WAVES",
+        artist: "Glass Animals",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "ABOUT YOU",
+        artist: "THE 1975",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Night Changes",
+        artist: "One Direction",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "MOOD ( SPEED UP )",
+        artist: "24k Goldn",
+        img: "music-11",
+        src: "music-11"
+    },
+];
